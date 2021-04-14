dd6a28642c6babf2a269aaa0b8a866b86a3b1cbd
Raikhana <raikhanaurazbaeva@gmail.com>
2021-04-13 23:00:41 -0600

    Add project files.

---
+++ DateTimeStructure/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace DateTimeStructure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFutureValue = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtYears = new System.Windows.Forms.TextBox();
+            this.txtInterestRate = new System.Windows.Forms.TextBox();
+            this.txtMonthlyInvestment = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBirthDate = new System.Windows.Forms.TextBox();
+            this.txtCurrentDate = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSendInformation = new System.Windows.Forms.TextBox();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFutureValue
+            // 
+            this.txtFutureValue.Location = new System.Drawing.Point(409, 100);
+            this.txtFutureValue.Name = "txtFutureValue";
+            this.txtFutureValue.ReadOnly = true;
+            this.txtFutureValue.Size = new System.Drawing.Size(147, 20);
+            this.txtFutureValue.TabIndex = 30;
+            this.txtFutureValue.TabStop = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(774, 195);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(67, 24);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(409, 128);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(70, 23);
+            this.btnCalculate.TabIndex = 7;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtYears
+            // 
+            this.txtYears.Location = new System.Drawing.Point(409, 72);
+            this.txtYears.Name = "txtYears";
+            this.txtYears.Size = new System.Drawing.Size(147, 20);
+            this.txtYears.TabIndex = 6;
+            this.txtYears.Text = "3";
+            // 
+            // txtInterestRate
+            // 
+            this.txtInterestRate.Location = new System.Drawing.Point(409, 45);
+            this.txtInterestRate.Name = "txtInterestRate";
+            this.txtInterestRate.Size = new System.Drawing.Size(147, 20);
+            this.txtInterestRate.TabIndex = 5;
+            this.txtInterestRate.Text = "2";
+            // 
+            // txtMonthlyInvestment
+            // 
+            this.txtMonthlyInvestment.Location = new System.Drawing.Point(409, 17);
+            this.txtMonthlyInvestment.Name = "txtMonthlyInvestment";
+            this.txtMonthlyInvestment.Size = new System.Drawing.Size(147, 20);
+            this.txtMonthlyInvestment.TabIndex = 4;
+            this.txtMonthlyInvestment.Text = "1000";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(297, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 20);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Future Value:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(297, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 20);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Number of Years:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(297, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 20);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Yearly Interest Rate: ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(297, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 20);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Monthly Investment:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtBirthDate
+            // 
+            this.txtBirthDate.Location = new System.Drawing.Point(122, 100);
+            this.txtBirthDate.Name = "txtBirthDate";
+            this.txtBirthDate.Size = new System.Drawing.Size(147, 20);
+            this.txtBirthDate.TabIndex = 3;
+            this.txtBirthDate.Text = "1958-07-07";
+            // 
+            // txtCurrentDate
+            // 
+            this.txtCurrentDate.Location = new System.Drawing.Point(122, 72);
+            this.txtCurrentDate.Name = "txtCurrentDate";
+            this.txtCurrentDate.ReadOnly = true;
+            this.txtCurrentDate.Size = new System.Drawing.Size(147, 20);
+            this.txtCurrentDate.TabIndex = 30;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(14, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 20);
+            this.label5.TabIndex = 33;
+            this.label5.Text = "Enter a birth date:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(14, 72);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 20);
+            this.label6.TabIndex = 32;
+            this.label6.Text = "Current date:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtSendInformation
+            // 
+            this.txtSendInformation.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSendInformation.Location = new System.Drawing.Point(577, 17);
+            this.txtSendInformation.Multiline = true;
+            this.txtSendInformation.Name = "txtSendInformation";
+            this.txtSendInformation.ReadOnly = true;
+            this.txtSendInformation.Size = new System.Drawing.Size(264, 168);
+            this.txtSendInformation.TabIndex = 36;
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(122, 45);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(147, 20);
+            this.txtPhoneNumber.TabIndex = 2;
+            this.txtPhoneNumber.Text = "(559) 440-9071";
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.Location = new System.Drawing.Point(122, 17);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(147, 20);
+            this.txtFullName.TabIndex = 1;
+            this.txtFullName.Text = "joel ray murach";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(14, 45);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(114, 19);
+            this.label7.TabIndex = 39;
+            this.label7.Text = "Enter phone number:";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(14, 17);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(114, 20);
+            this.label8.TabIndex = 38;
+            this.label8.Text = "Enter full name:";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(14, 128);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(26, 13);
+            this.label9.TabIndex = 41;
+            this.label9.Text = "Age";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(122, 128);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.ReadOnly = true;
+            this.txtAge.Size = new System.Drawing.Size(147, 20);
+            this.txtAge.TabIndex = 42;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(701, 195);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(67, 24);
+            this.btnSend.TabIndex = 9;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(486, 128);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(70, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 233);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Controls.Add(this.txtFullName);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtSendInformation);
+            this.Controls.Add(this.txtBirthDate);
+            this.Controls.Add(this.txtCurrentDate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtFutureValue);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtYears);
+            this.Controls.Add(this.txtInterestRate);
+            this.Controls.Add(this.txtMonthlyInvestment);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFutureValue;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtYears;
+        private System.Windows.Forms.TextBox txtInterestRate;
+        private System.Windows.Forms.TextBox txtMonthlyInvestment;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBirthDate;
+        private System.Windows.Forms.TextBox txtCurrentDate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSendInformation;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ DateTimeStructure/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Drawing.Printing;
+using System.Text.RegularExpressions;
+
+
+namespace DateTimeStructure
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, System.EventArgs e)
+        {
+            txtSendInformation.Text = "";
+            try
+            {
+                string number = txtInterestRate.Text.Trim();
+                string digitsOnly1 = "";
+                foreach (char c in number)
+                {
+                    if (!(c == '%'))
+                    {
+                        digitsOnly1 += c;
+                    }
+                }
+                txtInterestRate.Text = digitsOnly1;
+
+                if (Validatingform())
+                {
+                    decimal Investment = decimal.Parse(txtMonthlyInvestment.Text, NumberStyles.Currency);
+                    decimal InterestRate = decimal.Parse(txtInterestRate.Text, NumberStyles.AllowDecimalPoint | NumberStyles.AllowCurrencySymbol);
+                    int years = int.Parse(txtYears.Text, NumberStyles.None);
+
+                    int months = years * 12;
+                    decimal monthlyInterestRate = InterestRate / 12 / 100;
+                    decimal futureValue = CalculateFutureValue(
+                        Investment, monthlyInterestRate, months);
+
+                    decimal interestRatePercent = InterestRate / 100;
+                    txtMonthlyInvestment.Text = Investment.ToString("c");
+                    txtInterestRate.Text = interestRatePercent.ToString("p");
+                    txtYears.Text = years.ToString();
+                    txtFutureValue.Text = futureValue.ToString("c");
+                    txtMonthlyInvestment.Focus();
+
+                    string phoneNumber = txtPhoneNumber.Text.Trim(); //txtPhoneNumber.Text
+                    string digitsOnly = "";
+                    foreach (char c in phoneNumber)
+                    {
+                        if (!(
+                            c == '(' || c == ')' ||
+                            c == ' ' || c == '-' || c == '.'
+                            ))
+                        {
+                            digitsOnly += c;
+                        }
+                    }
+                    string standardFormat = digitsOnly.Insert(3, "-");
+                    standardFormat = standardFormat.Insert(7, "-");
+                    if (!Regex.Match(standardFormat, @"^[1-9]\d{2}-[1-9]\d{2}-\d{4}$").Success)
+                    {
+                        MessageBox.Show("Invalid phone number");
+                    }
+                    txtPhoneNumber.Text = standardFormat;
+
+                    string fullName = txtFullName.Text; // txtFullName.Text
+                    string[] names = fullName.Trim().Split(' ');
+
+                    string first = "";
+                    string middle = "";
+                    string last = "";
+
+                    if (names.Length == 1)
+                        first = names[0];
+                    else if (names.Length == 2)
+                    {
+                        first = names[0];
+                        last = names[1];
+                    }
+                    else if (names.Length > 2)
+                    {
+                        first = names[0];
+                        middle = names[1];
+                        last = names[2];
+                    }
+                    txtFullName.Text =  this.ToInitialCap(first) + " " +
+                                        this.ToInitialCap(middle) + " " +
+                                        this.ToInitialCap(last);
+                    string fillName =   this.ToInitialCap(first) + " " +
+                                        this.ToInitialCap(middle) + " " +
+                                        this.ToInitialCap(last);
+
+                    DateTime currentDate = DateTime.Today; // txtCurrentDate.Text
+                    txtCurrentDate.Text = currentDate.ToShortDateString();
+
+                    string dateString = txtBirthDate.Text;//txtBirthDate.Text;
+                    DateTime dateOut;
+                    if ((DateTime.TryParseExact(dateString, "yyyy-MM-dd",
+                                      new CultureInfo("en-US"),
+                                      DateTimeStyles.None,
+                                      out dateOut)))
+                    {
+                        txtBirthDate.Text = dateString;
+                        DateTime birthDate = DateTime.Parse(txtBirthDate.Text);
+                        int age = currentDate.Year - birthDate.Year; //txtAge
+                        if (currentDate.DayOfYear < birthDate.DayOfYear)
+                            age--;
+                        txtBirthDate.Text = birthDate.ToShortDateString();
+                        txtAge.Text = age.ToString();
+
+                        txtSendInformation.Text += "Full name:\t  " + fillName + "\r\n" +
+                                         "Phone number:\t  " + standardFormat + "\r\n" + "\r\n" +
+                                         "Current date:\t  " + currentDate.ToShortDateString() + "\r\n" +
+                                         "Birth date:\t  " + birthDate.ToShortDateString() +"\r\n" +
+                                         "Age:\t\t  " + age + "\r\n" + "\r\n" +
+                                         "Monthly Investment:  " + Investment.ToString("c") + "\r\n" +
+                                         "Yearly Interest Rate:  " + interestRatePercent.ToString("p") + "\r\n" +
+                                         "Number of Years:\t  " + years.ToString() + "\r\n" +
+                                         "Future Value:\t  " + futureValue.ToString("c") + "\r\n";
+                    }
+                    else
+                    {
+                        MessageBox.Show("A birth date string {0} is invalid.", dateString);
+                        txtBirthDate.Text = " ";
+                        txtInterestRate.Text = " ";
+                    }
+                }
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show(
+                    "An overflow exception. Please enter smaller values.",
+                    "Entry Error");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n\n" +
+                    ex.GetType().ToString() + "\n" +
+                    ex.StackTrace, "Exception");
+            }
+        }
+
+        private string ToInitialCap(string s)
+        {
+            if (s.Length > 0)
+            {
+                string initialCap = s.Substring(0, 1).ToUpper();
+                string lowerCaseLetters = s.Substring(1).ToLower();
+                s = initialCap + lowerCaseLetters;
+            }
+            return s;
+        }
+
+        public bool Validatingform()
+        {
+            return
+                Present(txtMonthlyInvestment, "Monthly Investment") &&
+                Currency(txtMonthlyInvestment, "Monthly Investment") &&
+                WithinRange(txtMonthlyInvestment, "Monthly Investment", 1, 1000) &&
+
+                Present(txtInterestRate, "Yearly Interest Rate") &&
+                Decimal(txtInterestRate, "Yearly Interest Rate") &&
+                WithinRange(txtInterestRate, "Yearly Interest Rate", 1, 20) &&
+
+                Present(txtYears, "Number of Years") &&
+                Int32(txtYears, "Number of Years") &&
+                WithinRange(txtYears, "Number of Years", 1, 40) &&
+
+                Present(txtBirthDate, "Enter a birth date:");
+        }
+
+        public bool Present(TextBox textBox, string name)
+        {
+            if (textBox.Text == "")
+            {
+                MessageBox.Show(name + " is a required field.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+        public bool Decimal(TextBox textBox, string name)
+        {
+            decimal number = 0m;
+            if (decimal.TryParse(textBox.Text, NumberStyles.Number, CultureInfo.CurrentCulture, out number))//
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(name + " must be a decimal value.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+        }
+        public bool Currency(TextBox textBox, string name)
+        {
+            decimal number = 0m;
+            if (decimal.TryParse(textBox.Text, NumberStyles.Currency, CultureInfo.CurrentCulture, out number))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(name + " must be in currency format.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+        }
+        public bool Int32(TextBox textBox, string name)
+        {
+            int number = 0;
+            if (int.TryParse(textBox.Text, NumberStyles.None, CultureInfo.CurrentCulture, out number))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(name + " must be an integer.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+        }
+        public bool WithinRange(TextBox textBox, string name,
+            decimal min, decimal max)
+        {
+            decimal number = decimal.Parse(textBox.Text, NumberStyles.Currency);
+            if (number < min || number > max)
+            {
+                MessageBox.Show(name + " must be between " + min +
+                    " and " + max + ".", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        private decimal CalculateFutureValue(decimal monthlyInvestment,
+            decimal interestRateMonthly, int months)
+        {
+            decimal futureValue = 0m;
+            for (int i = 0; i < months; i++)
+            {
+                futureValue = (futureValue + monthlyInvestment)
+                    * (1 + interestRateMonthly);
+            }
+            return futureValue;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            PrintDocument p = new PrintDocument();
+            p.PrintPage += delegate (object sender1, PrintPageEventArgs e1)
+            {
+                e1.Graphics.DrawString(txtSendInformation.Text, new Font("Times New Roman", 12),
+                                       new SolidBrush(Color.Black),
+                                       new RectangleF(0, 0, p.DefaultPageSettings.PrintableArea.Width, p.DefaultPageSettings.PrintableArea.Height));
+            };
+            try
+            {
+                p.Print();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Exception Occured While Printing", ex);
+            }
+        }
+
+        private void btnExit_Click(object sender, System.EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            txtMonthlyInvestment.Text = "";
+            txtInterestRate.Text = "";
+            txtYears.Text = "";
+            txtFutureValue.Text = "";
+
+            txtPhoneNumber.Text = "";
+            txtFullName.Text = "";
+            txtCurrentDate.Text = "";
+            txtBirthDate.Text = "";
+            txtAge.Text = "";
+
+            txtSendInformation.Text = "";
+        }
+    }
+}
